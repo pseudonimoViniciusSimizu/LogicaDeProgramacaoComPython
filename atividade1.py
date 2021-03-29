b63ecd55fd6d8bf72492c0570b610f55e1781424
pseudonimoViniciusSimizu <80995705+pseudonimoViniciusSimizu@users.noreply.github.com>
2021-03-29 12:38:26 -0300

    Update atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -1,1 +1,28 @@
-print("Olá professor")
+def converte_entrada (texto_dos_numeros):
+  lista = []
+  texto = texto_dos_numeros
+  y = (texto.split(' '))
+  for x in y:
+    x = int(x)
+    lista.append (x)
+  return lista
+  
+def processar_numeros (lista):
+  i = 0
+  soma = 0
+  while len(lista) > i:
+    soma += lista [i]
+    i += 1
+  return (soma, i)
+
+def main (lista_dos_numeros):
+  soma = soma_e_contagem [0]
+  contagem = soma_e_contagem [1]
+  media = soma / contagem
+  return media
+
+lista = converte_entrada (input (f"Coloque A Sua Entrada Aqui "))
+soma_e_contagem = processar_numeros (lista)
+media = main (lista)
+
+print (media)
