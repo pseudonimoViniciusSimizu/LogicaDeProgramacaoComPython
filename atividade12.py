22762e53837e57bab2be04b2029b5fa65ac234dc
pseudonimoViniciusSimizu <80995705+pseudonimoViniciusSimizu@users.noreply.github.com>
2021-04-25 14:30:43 -0300

    Create atividade12.py

---
+++ atividade12.py
@@ -0,0 +1,20 @@
+def exer1 ():
+    numbers_list = []
+    while len( numbers_list ) < 3:
+        numbers_list.append ( int ( input (f"{len(numbers_list) + 1}° Number: ")))
+    print(sorted(numbers_list)[ -1:])
+
+
+def exer2 ():
+    lista_de_palavras = ("Eu sou um aluno feliz".split())
+    # imagino eu que não era para imprimir e nem retornar :)
+
+
+def exer3 ():
+    list_sum = sum([ 3, 6, 3, 2, 1 ])
+    # aqui também
+  
+
+def exer4 ():
+    converted_list = (" ".join( [ "seu", "joao", "esta", "aqui" ] ))
+    # esse também
